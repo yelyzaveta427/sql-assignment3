137605ff7155e1f5732ac20a3a3c2057170eafb0
yelyzaveta <yelyzaveta.voitovych.25@kse.org.ua>
2026-06-25 22:19:46 +0300

    Adding procedure to create order

--- main-script.sql
+++ main-script.sql
@@ -10,3 +10,12 @@ begin
 end;
 $$ language plpgsql;
 
+
+create or replace procedure create_order(p_customer_id int)
+language sql
+as $$
+    insert into orders (customer_id)
+    select customer_id 
+    from customers 
+    where customer_id = p_customer_id;
+$$;
